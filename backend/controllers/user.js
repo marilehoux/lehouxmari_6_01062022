c7e072a574ea4150d4b75569fa5ea2467aabf2b3
marilehoux <mari.lehoux@gmail.com>
2022-06-28 07:52:59 +0200

    create folder backend npm init server.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -17,13 +17,14 @@ exports.signup = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
   };
   exports.login = (req, res, next) => {
+    console.log(req);
     User.findOne({ email: req.body.email })
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
         bcrypt.compare(req.body.password, user.password)
-          .then(valid => {
+          .then(valid => {            
             if (!valid) {
               return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
